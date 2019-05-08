27d1c315eca3be2c9c1610f1502b1ae28b616782
Jonathan Zheng <jz@dyn3202-132.wlan.ic.ac.uk>
2019-05-08 16:13:46 +0100

    pgd working, TODO conversion to routing graph

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
-    int I[] = { 0,0,1,1 };
-    int J[] = { 2,3,2,3 };
+    int I[] = { 0,0,0,1,1,1,2 };
+    int J[] = { 2,3,4,2,3,4,3 };
 
-    module* super = init_modules_undirected(4, 4, I, J);
+    module* super = init_modules_undirected(5, 7, I, J);
+    pgd(super);
     print_dfs(super);
 }
--- pgd.cpp
+++ pgd.cpp
@@ -2,25 +2,30 @@
 #include <unordered_set>
 #include <iostream>
 
+using std::vector;
+using std::unordered_set;
+
 struct module
 {
     int idx;
-    std::unordered_set<int> neighbours;
-    std::vector<module*> children; // can never lose children, so removal is not a problem
-    // TODO: check if should be a hashset instead
+
+    // TODO: check if these are faster with vectors instead
+    //       may also be better to not use a root module
+    unordered_set<module*> neighbours;
+    unordered_set<module*> children;
     module(int idx) : idx(idx), neighbours(), children() {}
 };
 
 module* init_modules_undirected(int n, int m, int* I, int* J)
 {
     // used to make graph undirected, in case it is not already
-    std::vector<std::unordered_set<int>> undirected(n);
+    vector<unordered_set<int>> undirected(n);
 
-    module* super = new module(-1);
-    super->children.reserve(n);
+    vector<module*> leaves;
+    leaves.reserve(n);
     for (int i=0; i<n; i++)
     {
-        super->children.push_back(new module(i));
+        leaves.push_back(new module(i));
     }
 
     for (int ij=0; ij<m; ij++)
@@ -33,23 +38,28 @@ module* init_modules_undirected(int n, int m, int* I, int* J)
         {
             undirected[i].insert(j);
             undirected[j].insert(i);
-            super->children[i]->neighbours.insert(j);
-            super->children[j]->neighbours.insert(i);
+            leaves[i]->neighbours.insert(leaves[j]);
+            leaves[j]->neighbours.insert(leaves[i]);
         }
     }
-    return super;
+    module* root = new module(-1);
+    for (int i=0; i<n; i++)
+    {
+        root->children.insert(leaves[i]);
+    }
+    return root;
 }
 
 void print_dfs(module* parent)
 {
     std::cout << parent->idx << ": ";
-    for (int neighbour : parent->neighbours)
+    for (auto neighbour : parent->neighbours)
     {
-        std::cout << neighbour << " ";
+        std::cout << neighbour->idx << " ";
     }
     std::cout << std::endl;
 
-    for (module* child : parent->children)
+    for (auto child : parent->children)
     {
         print_dfs(child);
     }
@@ -57,10 +67,10 @@ void print_dfs(module* parent)
 
 // almost exactly the same function as in the Dwyer paper,
 // except it simply returns the intersection
-int nedges(const std::unordered_set<int>& Nm, const std::unordered_set<int>& Nn)
+int nedges(const unordered_set<module*>& Nm, const unordered_set<module*>& Nn)
 {
     int num_intersect = 0;
-    for (int nm : Nm)
+    for (auto nm : Nm)
     {
         if (Nn.find(nm) != Nn.end())
         {
@@ -82,19 +92,19 @@ int nedges(const module* m, const module* n)
         return nedges(n->neighbours, m->neighbours);
     }
 }
-/*
-void pgd(module* super)
+void pgd(module* root)
 {
-    int new_module_idx = -2;
+    int new_module_idx = root->children.size(); // assumes a dense indexing
     while (true)
     {
         // find best merge
+        // TODO: store these in memory to reduce computation
         int best_reduction = 0;
         module* best1;
         module* best2;
-        for (module* child1 : super->children)
+        for (auto child1 : root->children)
         {
-            for (module* child2 : super->children)
+            for (auto child2 : root->children)
             {
                 if (child1->idx >= child2->idx) // don't do the same pair redundantly
                     continue;
@@ -104,6 +114,7 @@ void pgd(module* super)
                 
                 if (reduction > best_reduction)
                 {
+                    best_reduction = reduction;
                     best1 = child1;
                     best2 = child2;
                 }
@@ -112,27 +123,76 @@ void pgd(module* super)
         if (best_reduction == 0)
             break;
 
-        // check for full merges
+        // check for merge types
         bool merge1 = (best1->children.size()!=0) && (best1->neighbours.size()==best_reduction);
         bool merge2 = (best2->children.size()!=0) && (best2->neighbours.size()==best_reduction);
 
-        // do a full merge
-        if (merge1 && merge2)
+        // TODO: only iterate through the smaller sets in the following for loops
+        if (merge1 && merge2) // full merge
         {
-            // move all 2's edges into 1, then remove 2
+            // both neighbour sets are the same
+            // therefore just delete one of them, the other adopting their children
+            for (auto child : best2->children)
+            {
+                best1->children.insert(child);
+            }
+            root->children.erase(best2);
+            delete best2;
         }
         else if (merge1)
         {
-            // move all 2's shared edges into 1, then parent 1 to 2
+            // best1 will be left with no neighbours, and so is identical to the new parent
+            // therefore remove all shared edges in 2, then parent 2 to 1
+            for (auto neighbour : best1->neighbours)
+            {
+                best2->neighbours.erase(neighbour);
+            }
+            best1->children.insert(best2);
+            root->children.erase(best2);
         }
         else if (merge2)
         {
-            // move all 1's shared edges into 2, then parent 2 to 1
+            // best2 will be left with no neighbours, and so is identical to the new parent
+            // therefore remove all shared edges in 1, then parent 1 to 2
+            for (auto neighbour : best2->neighbours)
+            {
+                best1->neighbours.erase(neighbour);
+            }
+            best2->children.insert(best1);
+            root->children.erase(best1);
         }
-        else
+        else // new module
         {
             // create new module and move all shared edges to it, parent both to new
+            module* new_parent = new module(new_module_idx++); // all new indices are >n
+            for (auto neighbour : best1->neighbours)
+            {
+                if (best2->neighbours.find(neighbour) != best2->neighbours.end())
+                {
+                    // move all shared edges to new module
+                    neighbour->neighbours.erase(best1);
+                    neighbour->neighbours.erase(best2);
+                    neighbour->neighbours.insert(new_parent);
+                    new_parent->neighbours.insert(neighbour);
+                }
+            }
+            for (auto neighbour : new_parent->neighbours)
+            {
+                // remove from originals
+                best1->neighbours.erase(neighbour);
+                best2->neighbours.erase(neighbour);
+            }
+            // parent to new
+            new_parent->children.insert(best1);
+            new_parent->children.insert(best2);
+            root->children.insert(new_parent);
+            root->children.erase(best1);
+            root->children.erase(best2);
         }
     }
 }
-*/
+
+// void routing(module* root);
+// returns:
+// n, I, J, I2, J2
+// indices of tree edges, indices of power edges
